643af48dae40c2d7aed1c1d1388c949dd6331eef
Iqbal Kang <iqbal.kang.cohort225@gmail.com>
2022-07-31 19:41:24 -0700

    added new script file

--- app.js
+++ app.js
@@ -1,41 +1,70 @@
-'use sctrict';
+const equationEl = document.querySelector(".equation");
+const resultEl = document.querySelector(".result");
+const calculateEl = document.querySelector(".calculate");
+const numbers = document.querySelectorAll(".operand");
+const operators = document.querySelectorAll(".operator");
 
-const btns = document.querySelectorAll('.btn');
-const equationEl = document.querySelector('.equation');
-const resultEl = document.querySelector('.result');
-// const historyEl = document.querySelector('.history');
+let operand1 = "";
+let operand2 = "";
+let operation = "";
+let result;
 
-
-let str = '';
-btns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    
-    // Calculating
-    if (btn.textContent.trim() === '=') {
-      //result update
-      resultEl.textContent = eval(str);
-      str = '';
-    }
-    // Deleting
-    else if (btn.textContent.trim() === '←') {
-      str = str.slice(0, -1);
-      equationEl.textContent = str;
-    }
-    // Clearing
-    else if (btn.textContent.trim() === 'C') {
-      str = '';
-      equationEl.textContent = str;
-      resultEl.textContent = 0;
-    }
-    else{
-      str += btn.textContent;
-      // equation update
-      equationEl.textContent = str;
-    }
+numbers.forEach((num) => {
+  num.addEventListener("click", (e) => {
+    if (operand2.includes(".") && e.target.textContent === ".") return;
+    operand2 += e.target.textContent;
+    equationEl.textContent = operand1 + operation + operand2;
   });
 });
 
+const operationHandler = function (operator) {
+  if (operation) {
+    operation = operator;
+    let newEquationEl = equationEl.textContent.substring(
+      0,
+      equationEl.textContent.length - 1
+    );
+    newEquationEl += operation;
+    equationEl.textContent = newEquationEl;
+  } else if (result) {
+    operation = operator;
+    equationEl.textContent = result + operation;
+    operand1 = result;
+    operand2 = "";
+  } else {
+    operation = operator;
+    equationEl.textContent += operation;
+    operand1 = operand2;
+    operand2 = "";
+  }
+};
 
+operators.forEach((operator) => {
+  operator.addEventListener("click", (e) => {
+    operationHandler(e.target.textContent);
+  });
+});
 
+calculateEl.addEventListener("click", (e) => {
+  let num1 = Number(operand1);
+  let num2 = Number(operand2);
 
+  if (num1 && num2) {
+    if (operation === "+") {
+      result = num1 + num2;
+    } else if (operation === "-") {
+      result = num1 - num2;
+    } else if (operation === "*") {
+      result = num1 * num2;
+    } else if (operation === "/") {
+      result = num1 / num2;
+    } else if (operation === "%") {
+      result = num1 % num2;
+    }
 
+    resultEl.textContent = result;
+    operand1 = "";
+    operand2 = "";
+    operation = "";
+  }
+});
